ee13d8b50a0a1f01e4733afdc39d37bb9ec0018d
Anil Kulkarni <Anil Kulkarni@DESKTOP-AK0OO33>
2023-12-22 04:47:49 +0530

    first commit

---
+++ shruthi_helloworld/src/test/java/shruthi_helloworld/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package shruthi_helloworld;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld() {
+		System.out.println("...Hello World....");
+	}
+	
+	
+
+}
