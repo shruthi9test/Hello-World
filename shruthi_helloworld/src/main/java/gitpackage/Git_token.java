b52d770f9373d0fdeeefe2085d4744e352db5a0f
'shruthi9test' <'shruthi9test@gmail.com'>
2023-12-23 08:14:32 +0530

    added one java class under src/main/java

---
+++ shruthi_helloworld/src/main/java/gitpackage/Git_token.java
@@ -0,0 +1,10 @@
+package gitpackage;
+
+public class Git_token {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Gittoken: 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");
+	}
+
+}
